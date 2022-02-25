d5439649a51c7199f2d8bc21111de2f1ffd0bfdf
nicolasbernardet <nicolas.bernardet@live.fr>
2022-02-25 10:45:21 +0100

    Rename to security

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecurityLayoutComponent } from './layouts/security-layout/security-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecurityLayoutComponent
   ],
   imports: [
     BrowserModule,
